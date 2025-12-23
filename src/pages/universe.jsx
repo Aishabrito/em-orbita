ce193ba36ef18ce713c2cd124c89928e43b55b4a
AishaBrito <aisha.paola.20221@poli.ufrj.br>
2025-12-23 15:01:28 -0300

    feat: adiciona feedback sonoro (SFX) ao completar e interagir com hábitos

--- src/pages/universe.jsx
+++ src/pages/universe.jsx
@@ -3,6 +3,7 @@ import { useCosmicHabits } from '../hooks/useCosmicHabits';
 import Planet from '../components/planet';
 import ModalCriarHabito from '../components/modals/modalCriarHabito.jsx';
 import EstrelaCentral from '../components/estrelaCentral';
+import { getToday } from '../utils/date'; 
 import { 
   FundoCosmico, 
   CabecalhoUniverso, 
@@ -10,6 +11,8 @@ import {
   EstadoVazio, 
   BotaoNovoHabito 
 } from '../components/componentsUniverse';
+import successSfx from '../assets/success.mp3';
+import clickSfx from '../assets/click.mp3';
 
 const Universe = () => {
   // Lógica e Estado
@@ -17,17 +20,45 @@ const Universe = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isEditMode, setIsEditMode] = useState(false);
 
-  // Handlers
+  // audios
+  const playSuccess = new Audio(successSfx);
+  const playClick = new Audio(clickSfx);
+  playSuccess.volume = 0.5; 
+  playClick.volume = 0.3;
+
   const handleInteraction = (id) => {
-    if (isEditMode) deleteHabit(id);
-    else incrementStreak(id);
+    // MODO DESTRUIÇÃO 
+    if (isEditMode) {
+      deleteHabit(id);
+      playClick.currentTime = 0;
+      playClick.play();
+      return;
+    }
+
+    // MODO NORMAL (Verifica o que vai acontecer antes de atualizar)
+    const habit = habits.find(h => h.id === id);
+    const today = getToday();
+    const isDoneToday = habit.history.includes(today);
+
+    if (!isDoneToday) {
+      // --> Vai Completar: Toca som de VITÓRIA 🔊
+      playSuccess.currentTime = 0;
+      playSuccess.play().catch(e => console.log("Audio blocked:", e));
+    } else {
+      // --> Vai Desmarcar: Clique simples 🔊
+      playClick.currentTime = 0;
+      playClick.play().catch(e => console.log("Audio blocked:", e));
+    }
+
+    //  Executa a lógica original
+    incrementStreak(id);
   };
 
   const totalStreak = habits ? habits.reduce((acc, h) => acc + h.streak, 0) : 0;
   const temHabitos = habits && habits.length > 0;
 
   return (
-    <div className="relative flex items-center justify-center w-full h-screen overflow-hidden bg-[#0B0B15] text-white">
+    <div className="relative flex items-center justify-center w-full h-screen overflow-hidden bg-[#0B0B15] text-white z-0">
       
       {/* Visual Estático */}
       <FundoCosmico />
@@ -57,7 +88,7 @@ const Universe = () => {
         />
       ))}
 
-      {/* Botão de Ação  */}
+      {/* Botão de Ação */}
       {!isEditMode && (
         <BotaoNovoHabito onClick={() => setIsModalOpen(true)} />
       )}
